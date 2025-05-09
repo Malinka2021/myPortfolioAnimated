b884d21a6a3495d7756eccc9ed6ed66914e16124
Alina Pellja <alina pellja@gmail.com>
2025-05-09 11:14:00 +0300

    mobile gallery interactive works

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ const btnHamburger = document.querySelector('.hamburger-menu');
 const menuPanel = document.querySelector('.menu-panel');
 const menuLinks = document.querySelectorAll('.menu-panel a');
 const btnCloseWindow = document.querySelector('.menu-panel button')
+const frameImage = document.querySelector('#frame');
+const images = [
+    'images/Gallery.jpg',
+    'images/cacti.webp',
+    'images/guitar.webp',
+    'images/laptop.webp',
+    'images/webdev.webp',
+    'images/aloe.jpeg'];
+
+let currentImageIndex = 0;
+// Set the initial image
+
+frameImage.addEventListener('click', () => {
+    //const randomIndex = Math.floor(Math.random() * images.length);
+    //frameImage.src = images[randomIndex];
+
+    currentImageIndex++;  // +1 loop
+
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0; // Reset to the first image
+    }
+        
+    frameImage.src = images[currentImageIndex]; // Set the new image
+}
+);
 
 btnHamburger.addEventListener('click', (event) => {
     menuPanel.classList.toggle('active');
