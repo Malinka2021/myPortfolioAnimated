29589bc1b6593770a5e38c9d7bbf8f41bc60aea0
Alina Pellja <alina pellja@gmail.com>
2025-05-09 08:33:16 +0300

    button js done

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const btnHamburger = document.querySelector('.hamburger-menu');
 const menuPanel = document.querySelector('.menu-panel');
 const menuLinks = document.querySelectorAll('.menu-panel a');
+const btnCloseWindow = document.querySelector('.menu-panel button')
 
 btnHamburger.addEventListener('click', (event) => {
-    
     menuPanel.classList.toggle('active');
-    
  }
 );
+
 document.addEventListener('click', (event) => {
     if (!menuPanel.contains(event.target) && !btnHamburger.contains(event.target)) {
         menuPanel.classList.remove('active');
@@ -21,3 +21,7 @@ menuLinks.forEach(link => {
     });
 }
 );
+
+btnCloseWindow.addEventListener('click', () => {
+    menuPanel.classList.remove('active');
+});
